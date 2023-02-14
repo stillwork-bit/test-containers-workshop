eb22ad00eb14cf825e9eeec2e9cabec6f84be1b2
Tereshin Andrey <Atereshin@inno.tech>
2023-02-15 00:27:20 +0300

    first commit

--- src/test/java/com/example/demo/api/RatingsControllerTest.java
+++ src/test/java/com/example/demo/api/RatingsControllerTest.java
@@ -1,12 +1,7 @@
-# Step 7: Test the API
-
-Now let's create a test for our API which will verify the business logic.
-
-```java
 package com.example.demo.api;
 
-import com.example.demo.model.Rating;
 import com.example.demo.support.AbstractIntegrationTest;
+import com.example.demo.model.Rating;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -15,6 +10,7 @@ import static org.hamcrest.Matchers.is;
 
 public class RatingsControllerTest extends AbstractIntegrationTest {
 
+    //Шаг 7. API автотесты для проверки логики тестового приложения
     @Test
     public void testRatings() {
         String talkId = "testcontainers-integration-testing";
@@ -68,28 +64,3 @@ public class RatingsControllerTest extends AbstractIntegrationTest {
                 .statusCode(404);
     }
 }
-```
-
-Run it, and it will fail.
-
-Why?
-
-There is no Kafka!
-
-Running Kafka in Docker is easy with Testcontainers.
-There is a Testcontainers module providing integration with Kafka and the `KafkaContainer` abstraction for your code.
-
-Just add it the same way as you added Redis and set the `spring.kafka.bootstrap-servers` system property.
-
-## Hint 1:
-
-Some containers expose helper methods. Check if there is one on `KafkaContainer` which might help you.
-
-## Hint 2:
-
-You can start several containers in parallel by doing:
-
-```java
-Stream.of(redis, kafka).parallel().forEach(GenericContainer::start);
-```
-
---
+++ src/test/java/com/example/demo/support/AbstractIntegrationTest.java
@@ -0,0 +1,71 @@
+package com.example.demo.support;
+
+import static io.restassured.RestAssured.given;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.specification.RequestSpecification;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.utility.MountableFile;
+
+//Шаг 3. Создание тестового класса AbstractIntegrationTest
+//@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+
+//Шаг 4. Подключение Postgres
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, properties = {
+        "spring.datasource.url=jdbc:tc:postgresql:14-alpine://testcontainers/workshop"
+})
+public class AbstractIntegrationTest {
+
+    protected RequestSpecification requestSpecification;
+
+    @LocalServerPort
+    protected int localServerPort;
+
+    static final GenericContainer redis = new GenericContainer("redis:6-alpine")
+            .withExposedPorts(6379);
+
+    static final KafkaContainer kafka = new KafkaContainer(
+            DockerImageName.parse("confluentinc/cp-kafka:5.4.3"));
+
+    static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:14-alpine")
+            .withCopyFileToContainer(MountableFile.forClasspathResource("/schema.sql"), "/docker-entrypoint-initdb.d/");
+
+    @DynamicPropertySource
+    public static void configureRedis(DynamicPropertyRegistry registry) {
+        Stream.of(redis, kafka, postgres).parallel().forEach(GenericContainer::start);
+        registry.add("spring.redis.host", redis::getHost);
+        registry.add("spring.redis.port", redis::getFirstMappedPort);
+        registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
+//        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+//        registry.add("spring.datasource.username", postgres::getUsername);
+//        registry.add("spring.datasource.password", postgres::getPassword);
+//        registry.add("spring.datasource.initialization-mode", () -> "never");
+    }
+
+    @BeforeEach
+    public void setUpAbstractIntegrationTest() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        requestSpecification = new RequestSpecBuilder()
+                .setPort(localServerPort)
+                .addHeader(
+                        HttpHeaders.CONTENT_TYPE,
+                        MediaType.APPLICATION_JSON_VALUE
+                )
+                .build();
+    }
+}
